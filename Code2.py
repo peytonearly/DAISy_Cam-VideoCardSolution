d7c67fafd12491a6c12f98e1b9fd36031e5d7650
KetanKamat-5 <59976265+KetanKamat-5@users.noreply.github.com>
2022-11-01 20:41:27 -0600

    Add files via upload

---
+++ Code2.py
@@ -0,0 +1,23 @@
+# Video Compression Code
+# Date: 11/1/2022
+# Take in Images frame by frame and Compress by a Certain Factor
+import math
+import PIL
+from PIL import Image
+import cv2
+import pandas as pd
+
+vidcap = cv2.VideoCapture('Team14_FinalRecording_Trim.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  scale_percent = 60 # percent of original size
+  width = int(image.shape[1] * scale_percent / 100)
+  height = int(image.shape[0] * scale_percent / 100)
+  dim = (width, height)
+  #resize image
+  resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+  print('Read a new frame: ', success)
+  count += 1
