84f2ca2e65c37a46810f60b42f3b4c48f3d1f6a1
peytonearly <peyton.early@colorado.edu>
2022-11-02 14:43:59 -0600

    More work on compression algorithm
    Output files seem to have random frame placed in. Live video capture file doesn't like my installed version of h.264 codec.

--- ComponentTestingScripts/Live_Compression.py
+++ ComponentTestingScripts/Live_Compression.py
@@ -9,7 +9,7 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 
 # Create VideoCapture object
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
 
 if not cam.isOpened():
     print("Unable to open camera. Exiting program.")
@@ -20,8 +20,8 @@ frame_width = int(cam.get(3))
 frame_height = int(cam.get(4))
 
 # Define VideoWriter object for compressed and uncompressed
-out = cv2.VideoWriter(path+"\\UncompressedLiveVideo.mp4", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (frame_width, frame_height))
-out_comp = cv2.VideoWriter(path+"\\CompressedLiveVideo.mp4", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (frame_width, frame_height))
+out = cv2.VideoWriter(path+"\\UncompressedLiveVideo.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 20, (frame_width, frame_height))
+out_comp = cv2.VideoWriter(path+"\\CompressedLiveVideo.mp4", cv2.VideoWriter_fourcc(*'mpv4'), 20, (frame_width, frame_height))
 
 while(True):
     ret, frame = cam.read()
@@ -31,4 +31,24 @@ while(True):
         out.write(frame)
         
         # Convert, compress, and save image
-        pil_image
+        # pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_YUV2RGB))
+        out_comp.write(frame)
+        
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        
+        # Press Q on keyboard to stop recording
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # Break the loop
+    else:
+        break
+
+# When everything is finished, release video capture and video write objects
+cam.release()
+out.release()
+out_comp.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
