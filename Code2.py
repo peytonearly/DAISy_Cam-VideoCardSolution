b4423e766426a947e2b70e6c863a4f96379d91ed
KetanKamat-5 <59976265+KetanKamat-5@users.noreply.github.com>
2022-11-02 11:57:14 -0600

    Add files via upload
    Adds a folder

--- Code2.py
+++ Code2.py
@@ -7,11 +7,11 @@ from PIL import Image
 import cv2
 import pandas as pd
 
-vidcap = cv2.VideoCapture('Team14_FinalRecording_Trim.mp4')
+vidcap = cv2.VideoCapture('Real_Slim_Shady.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("Compressed_Images_Folder/frame%d.jpg" % count, image)     # save frame as JPEG file      
   success,image = vidcap.read()
   scale_percent = 60 # percent of original size
   width = int(image.shape[1] * scale_percent / 100)
