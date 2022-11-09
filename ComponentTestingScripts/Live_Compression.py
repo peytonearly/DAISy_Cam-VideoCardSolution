3eff51ae343375c5f852f3df1c455f7a0fa31e9d
peytonearly <peyton.early@colorado.edu>
2022-11-08 17:55:36 -0700

    Updated testing codes

--- ComponentTestingScripts/Live_Compression.py
+++ ComponentTestingScripts/Live_Compression.py
@@ -1,5 +1,10 @@
 # Inspired from https://learnopencv.com/read-write-and-display-a-video-using-opencv-cpp-python/
 
+# Notes:
+# Compression doesn't seem to actually be compressing the images properly
+#   Look into trying a new technique using PIL since that seemed to work
+# When compression is run during stream, fps lags very badly
+
 import cv2
 import numpy as np
 from PIL import Image
@@ -20,22 +25,27 @@ frame_width = int(cam.get(3))
 frame_height = int(cam.get(4))
 
 # Define VideoWriter object for compressed and uncompressed
-out = cv2.VideoWriter(path+"\\UncompressedLiveVideo.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 20, (frame_width, frame_height))
-out_comp = cv2.VideoWriter(path+"\\CompressedLiveVideo.mp4", cv2.VideoWriter_fourcc(*'mpv4'), 20, (frame_width, frame_height))
+out = cv2.VideoWriter(path+"\\UncompressedLiveVideo.avi", cv2.VideoWriter_fourcc(*'MJPG'), 20, (frame_width, frame_height))
+out_comp = cv2.VideoWriter(path+"\\CompressedLiveVideo.avi", cv2.VideoWriter_fourcc(*'MJPG'), 20, (frame_width, frame_height))
+print("Video files created.")
 
 while(True):
     ret, frame = cam.read()
     
     if ret:
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        
         # Write uncompressed frame into file
         out.write(frame)
         
         # Convert, compress, and save image
         # pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_YUV2RGB))
+        # encode_jp2 = [int(cv2.IMWRITE_JPEG2000_COMPRESSION_X1000), 300] # Set jpeg2000 compression ratio to 30%
+        # res, img_comp = cv2.imencode('.jp2', frame, encode_jp2)
+        # img_dec = cv2.imdecode(img_comp, cv2.IMREAD_COLOR)
+        # out_comp.write(img_dec)
         out_comp.write(frame)
-        
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
         
         # Press Q on keyboard to stop recording
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -47,7 +57,7 @@ while(True):
 
 # When everything is finished, release video capture and video write objects
 cam.release()
-out.release()
+# out.release()
 out_comp.release()
 
 # Closes all the frames
