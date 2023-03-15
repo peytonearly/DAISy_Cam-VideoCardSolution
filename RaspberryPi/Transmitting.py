59cc1979beb26566be116d07f5fcb075fb78ecc6
Peyton Early <peyton.early@colorado.edu>
2023-03-14 19:37:16 -0600

    Test code for transmitting

--- RaspberryPi/Transmitting.py
+++ RaspberryPi/Transmitting.py
@@ -1,4 +1,5 @@
 # Import Python libraries
+import socket
 
 # Import custom functions
 from Flag import FLAG
@@ -11,4 +12,30 @@ def Transmit(flags: FLAG):
 ### Testing ###
 # Will run if this file is called #
 if __name__ == "__main__":
-    pass
+    ####################
+    ### From ChatGPT ###
+    ####################
+
+    # create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect the socket to the server's address and port
+    server_address = ('localhost', 10000)
+    print('connecting to %s port %s' % server_address)
+    sock.connect(server_address)
+
+    # send a message
+    message = b'This is beans!'
+    print('sending "%s"' % message)
+    sock.sendall(message)
+
+    # receive the response and print it
+    data = sock.recv(1024)
+    print('received "%s"' % data)
+
+    # clean up the socket
+    sock.close()
+
+    ####################
+    ### From ChatGPT ###
+    ####################
