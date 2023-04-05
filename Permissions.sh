9f52961baa6c1e586bfeaea6f57d71dad74f92fe
Peyton Early <peyton.early@colorado.edu>
2023-04-05 13:47:43 -0600

    Added permission bash script

---
+++ Permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script gives permissions for all directory folders
+
+chmod -R 777 $PWD"/RaspberryPi"
+chmod -R 777 $PWD"/Arduino"
+chmod -R 777 $PWD"/OBC"
+chmod -R 777 $PWD"/_Proto"
+chmod -R 777 $PWD"/Test_Results"
