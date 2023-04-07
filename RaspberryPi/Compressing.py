449fd6559fb6afed3b1f0aca152dcf3d7ba17c7e
Peyton Early <64031475+peytonearly@users.noreply.github.com>
2023-04-06 21:06:39 -0600

    Compression now runs in a separate thread properly

--- RaspberryPi/Compressing.py
+++ RaspberryPi/Compressing.py
@@ -14,42 +14,49 @@ def Compression(flags: FLAG):
     # Select next image file to compress
     #   Determine based on last file saved to Compressed folder
     compFiles = sorted([str(x) for x in flags.c.iterdir()]) # Create sorted list of files in Compressed folder
+    # compFiles = sorted()
+    
     if not len(compFiles):
         day = date.today().strftime("%m%d") # Find current mmdd
         todayUStr = day + "U000000.png" # Create image name string from date and naming standard
-        todayCStr = day + "U000000.png" # Create image name string from date and naming standard
-        nextComp = flags.c / todayCStr
-        uncompNext = flags.u / todayUStr
+        todayCStr = day + "C000000.png" # Create image name string from date and naming standard
+        nextCPath = flags.c / todayCStr
+        nextUPath = flags.u / todayUStr
     else:
-        mostRecent = compFiles[len(compFiles)-1] # Find most recent folder placed in Compressed folder
-        uncompNext = mostRecent[len(mostRecent)-15:(len(mostRecent)-11)] + 'U' + str(int(mostRecent[(len(mostRecent)-10):len(mostRecent)-4])+1).zfill(6) + mostRecent[len(mostRecent)-4:]
-        nextComp = uncompNext[:len(uncompNext)-11] + 'C' + uncompNext[len(uncompNext)-10:]
+        currComp = compFiles[len(compFiles)-1] # Find most recent folder placed in Compressed folder
+        nextUPath = currComp[len(currComp)-15:(len(currComp)-11)] + 'U' + str(int(currComp[(len(currComp)-10):len(currComp)-4])+1).zfill(6) + currComp[len(currComp)-4:]
+        nextCPath = nextUPath[:len(nextUPath)-11] + 'C' + nextUPath[len(nextUPath)-10:]
 
         # Create paths based on file names
-        uncompNext = flags.u / uncompNext
-        nextComp = flags.c / nextComp
+        nextUPath = flags.u / nextUPath
+        nextCPath = flags.c / nextCPath
 
     # Continue if file has been created 
-    if Path(uncompNext).exists():
+    if Path(nextUPath).exists():
 
         # Load frame from uncompressed folder
-        image = cv2.imread(str(uncompNext))
+        image = cv2.imread(str(nextUPath))
         
         # Set compression method parameters
-        if flags.METHOD == 0: # No compression method
+        if flags.METHOD == 0: 
+            # No compression method
             # params = [cv2.IMWRITE_PNG_STRATEGY_FIXED, flags.COMPQUAL]
             params = []
-        elif flags.METHOD == 1: # JPEG2000
+        elif flags.METHOD == 1: 
+            # JPEG2000
             params = [cv2.IMWRITE_JPEG2000_COMPRESSION_X1000, flags.COMPQUAL]
-        elif flags.METHOD == 2: # Run-length encoding
+        elif flags.METHOD == 2: 
+            # Run-length encoding
             params = [cv2.IMWRITE_PNG_STRATEGY_RLE, flags.COMPQUAL]
-        elif flags.METHOD == 3: # Dynamic Huffman encoding
+        elif flags.METHOD == 3: 
+            # Dynamic Huffman encoding
             params = [cv2.IMWRITE_PNG_STRATEGY_HUFFMAN_ONLY, flags.COMPQUAL]
-        elif flags.METHOD == 4: # Static Huffman encoding
+        elif flags.METHOD == 4: 
+            # Static Huffman encoding
             params = [cv2.IMWRITE_PNG_STRATEGY_FIXED, flags.COMPQUAL]
 
         # Save compressed image
-        cv2.imwrite(str(nextComp), image, params)
+        cv2.imwrite(str(nextCPath), image, params)
     return
 
 ### Testing ###
--- RaspberryPi/Main.py
+++ RaspberryPi/Main.py
@@ -39,6 +39,7 @@ def CompProcess():
 if __name__ == "__main__":
     # Initialize classes and variables
     flags = FLAG()
+    flags.METHOD = 0
     flags.RECORD = 1
     compProcess = mp.Process()
 
@@ -59,34 +60,32 @@ if __name__ == "__main__":
 
         # Determine if frame should be saved
         if ret and flags.RECORD:
-            # recProcess = mp.Process(target=SaveFrame, args=[flags, frame])
-            # recProcess.start()
-            # print("Running Saving.SaveFrame() with PID ", recProcess.pid)
-            # recProcess.join()
             SaveFrame(flags, frame)
 
+        # Determine if compression should run
         if not flags.EQUAL:
-            pass
-
-        # if not flags.EQUAL:
-        #     if not compProcess.exitcode and not compProcess.is_alive(): # Process hasn't exited and isn't running -- hasn't been started
-        #         # Start a new process
-        #         compProcess = mp.Process(target=Compression, args=[flags])
-        #         compProcess.start()
-        #         print("Process ", compProcess.pid, " started..")
-        #     elif not compProcess.exitcode and compProcess.is_alive(): # Process hasn't exited but is running
-        #         # Do nothing
-        #         print("-- Process ", compProcess.pid, " still running. --")  # Debug statement
-        #     elif compProcess.exitcode == 0: # Process has exited
-        #         # Exited without error
-        #         print("Process ", compProcess.pid, " finished successfully.")
-        #         compProcess = mp.Process(target=Compression, args=[flags])
-        #         compProcess.start()
-        #         print("Process ", compProcess.pid, " started.")
-        #     elif compProcess.exitcode > 0: # Process exited with error
-        #         print("== ERROR: Process ", compProcess.pid, " terminated with error ", compProcess.exitcode, " ==")
-        #     else:
-        #         print("Shouldn't ever get here")
-
-    # # Wait for subprocesses to finish before proceding
-    # recProcess.join()
+            if compProcess.exitcode is None and not compProcess.is_alive(): 
+                # Process hasn't exited and isn't running
+                # Start a new process
+                compProcess = mp.Process(target=Compression, args=[flags])
+                compProcess.start()
+                print("Process ", compProcess.pid, " started..")
+            elif compProcess.is_alive():
+                # Process hasn't exited but is running 
+                # Do nothing
+                # print("-- Process ", compProcess.pid, " still running. --")  # Debug statement
+                pass
+            elif not compProcess.is_alive() and compProcess.exitcode == 0: 
+                # Process has exited without error
+                compProcess.join()
+                print("Process ", compProcess.pid, " finished successfully.")
+                
+                # Create a new process
+                compProcess = mp.Process(target=Compression, args=[flags])
+                compProcess.start()
+                print("Process ", compProcess.pid, " started.")
+            elif compProcess.exitcode > 0: 
+                # Process exited with error
+                print("== ERROR: Process ", compProcess.pid, " terminated with error ", compProcess.exitcode, " ==")
+            else:
+                print("Shouldn't ever get here")
